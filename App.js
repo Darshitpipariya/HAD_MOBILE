1c9a778554fb8bb9a24e56801462b43deaf1eaf5
Darshitpipariya <darshit2272000@gmail.com>
2023-03-31 22:59:58 +0530

    Statusbar added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AuthProvider from './context/AuthContext';
 import AppNav from './screens/AppNav';
+import { StatusBar } from 'expo-status-bar';
 export default function App() {
   return (
 
     <AuthProvider>
+      <StatusBar style="auto"/>
       <AppNav />
     </AuthProvider>
   );
